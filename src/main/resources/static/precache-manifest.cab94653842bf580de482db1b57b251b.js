9a12ecf2b75bf8c2d37a262ae2252a7a6b5b2915
Tom Lam <tomlam0828@gmail.com>
2020-07-17 14:39:28 -0700

    deploy

---
+++ src/main/resources/static/precache-manifest.cab94653842bf580de482db1b57b251b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b11c3b003f8262486243e5b279c2048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcf8c5b6b4a35dd4898",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "678598be88e91bc9671a",
+    "url": "/static/css/main.87987d25.chunk.css"
+  },
+  {
+    "revision": "1fcf8c5b6b4a35dd4898",
+    "url": "/static/js/2.ddd7d11e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ddd7d11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678598be88e91bc9671a",
+    "url": "/static/js/main.a1891dc2.chunk.js"
+  },
+  {
+    "revision": "810289cf728f03fe18d5",
+    "url": "/static/js/runtime-main.436b851f.js"
+  }
+]);
